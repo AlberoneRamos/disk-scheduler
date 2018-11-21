6edbade1e9ed837f044752dcaffbde26d65bb507
Henrique Ramos <rickeight@hotmail.com>
2018-11-21 01:49:14 -0200

    Novo branch para utilizar Eclipse

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,10 @@ import classes.DiskScheduler;
 public class Main {
 
     public static void main(String[] args) {
-        DiskScheduler scheduler = new DiskScheduler("./in.txt");
-        scheduler.executeAllMethods();
+        DiskScheduler scheduler = new DiskScheduler();
+        for(int i = 1 ; i <= 4 ; i++) {
+        	scheduler.changeInputData("./input/Arquivo "+i+".in");
+        	scheduler.executeAllMethods("Arquivo "+i+".out");
+        }
     }
 }
